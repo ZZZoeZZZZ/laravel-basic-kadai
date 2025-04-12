04d4a763ec364f973a9b87f1fcd241894a118363
ZZZoeZZZZ <krt.emp6kkd@gmail.com>
2025-04-12 16:31:44 +0900

    バリデーション課題

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
+use App\Http\Requests\PostStoreRequest;
 
 
 class PostController extends Controller
@@ -17,5 +18,19 @@ class PostController extends Controller
     public function show($id) {
         $post = Post::find($id);
         return view('posts.show', compact('post'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(PostStoreRequest $request) {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return redirect("/posts");
+
     }
 }
